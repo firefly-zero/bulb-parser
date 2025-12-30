65607430f1c450ad4be26e82eeee3bec989aef8e
gram <git@orsinium.dev>
2025-12-30 13:36:39 +0100

    more stuff

--- src/types.rs
+++ src/types.rs
@@ -1,19 +1,21 @@
+use alloc::boxed::Box;
 use alloc::string::String;
-use alloc::vec::Vec;
 
 type ID = usize;
 
 pub struct Sections {
-    pub rooms: Vec<Room>,
-    pub tiles: Vec<Tile>,
-    pub images: Vec<Image>,
-    pub actions: Vec<Action>,
+    pub rooms: Box<Room>,
+    pub tiles: Box<Tile>,
+    pub images: Box<Image>,
+    pub actions: Box<Box<[Action]>>,
+    pub player: Image,
     pub n_vars: usize,
 }
 
 pub struct Room {
     pub tiles: [[ID; 30]; 20],
 }
+
 pub struct Tile {
     pub image: Option<ID>,
     pub wall: bool,
@@ -25,12 +27,7 @@ pub struct Image {
     pub raw: [u8; 64],
 }
 
-pub struct Action {
-    pub ado: Do,
-    pub next: Option<ID>,
-}
-
-pub enum Do {
+pub enum Action {
     /// Print a dialog line.
     Say(String),
     /// Pick up the tile as an item.
@@ -44,23 +41,36 @@ pub enum Do {
     Exit(ID, u8, u8),
     /// Replace the given tile in the current room.
     Tile(ID, u8, u8),
-    /// If the condition is true, execute a different action.
+    /// If the condition is true, clear the stack and execute a different action.
     Branch(Cond, Option<ID>),
     /// Assign the given value to the variable.
     Set(ID, i32),
+    /// Add the given value to the variable.
+    ///
+    /// Negative values can be used to substract.
+    Add(ID, i32),
+    /// Push an action at the end of the stack.
+    Enqueue(ID),
 }
 
 pub struct Cond {
     pub lhs: ID,
-    pub op: Operator,
+    pub cmp: Cmp,
     pub rhs: i32,
 }
 
-pub enum Operator {
+/// Comparison operator.
+pub enum Cmp {
+    /// `<`: Less than.
     Lt,
+    /// `<=`: Less than or equal.
     Lte,
+    /// `>`: Greater than.
     Gt,
+    /// `>=`: Greater than or equal.
     Gte,
+    /// `==`: Equal.
     Eq,
+    /// `!=`: Not equal.
     Ne,
 }
